0b68bd1084fc876c4de503d0b7be62278a41484b
Filipe Ribeiro <filipepaiva240@gmail.com>
2019-12-08 14:23:15 -0200

    Implements fetching data from clarifai API

--- src/App.js
+++ src/App.js
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import ColorsRecognition from "./components/ColorsRecognition/ColorsRecognition";
+import Clarifai from "clarifai";
 import "./App.scss";
 
+const API_KEY = process.env.REACT_APP_CLARIFAI_KEY;
+
+const app = new Clarifai.App({
+  apiKey: API_KEY
+});
+
 function App() {
+  const [input, setInput] = useState("");
+  const [isDetecting, setIsDetecting] = useState(false);
+
+  const onInputChange = event => {
+    setInput(event.target.value);
+  };
+
+  const onButtonSubmit = () => {
+    setIsDetecting(!isDetecting);
+  };
+
+  useEffect(() => {
+    app.models
+      .predict(
+        Clarifai.COLOR_MODEL,
+        "https://d1csarkz8obe9u.cloudfront.net/posterpreviews/colorful-background-design-template-f7d5ea9cc27d1719dfa78f67b1e00dfc_screen.jpg?ts=1561389258"
+      )
+      .then(response => console.log(response))
+      .catch(err => console.log(err));
+  }, [isDetecting]);
+
   return (
     <main className="App">
-      <ImageLinkForm />
+      <ImageLinkForm
+        onInputChange={onInputChange}
+        onButtonSubmit={onButtonSubmit}
+      />
       <ColorsRecognition />
     </main>
   );
--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -1,15 +1,18 @@
 import React from "react";
 import "./ImageLinkForm.scss";
 
-const ImageLinkForm = () => {
+const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <section className="ImageLinkForm">
       <input
         className="img-input"
         type="text"
         placeholder="Enter an image URL"
+        onChange={onInputChange}
       />
-      <button className="detect-btn">Detect Colors</button>
+      <button className="detect-btn" onClick={onButtonSubmit}>
+        Detect Colors
+      </button>
     </section>
   );
 };
